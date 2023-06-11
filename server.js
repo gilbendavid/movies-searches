877793e8b9627d44030a7951e6faa2b064df47aa
Gil Ben-David <gbendavid@apartmentlist.com>
2023-06-11 15:49:15 +0300

    server start + logs

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const port = 5555;
 
 app.get("/", (req, res) => {
   res.send("Movies Searches Server is Up!!!");
+  console.log("Movies Searches Server is Up!!!");
 });
 
 app.get("/getAllMoviesTitles", async (req, res) => {
@@ -19,6 +20,9 @@ app.get("/getAllMoviesTitles", async (req, res) => {
     return;
   }
   const result = await getAllMoviesTitles(req.query.searchValue);
+  console.log(
+    `getAllMoviesTitles searchValue=${req.query.searchValue} - Result=${result}`
+  );
   res.status(200).send({ result });
 });
 
@@ -28,6 +32,11 @@ app.get("/getMovieReleasedDate", async (req, res) => {
     return;
   }
   const result = await getMovieReleasedDate(req.query.identifier);
+  console.log(
+    `getMovieReleasedDate identifier=${
+      req.query.identifier
+    } - Result=${JSON.stringify(result)}`
+  );
   res.status(200).send({ result });
 });
 
@@ -37,9 +46,26 @@ app.post("/getHighestRatingMovie", async (req, res) => {
     return;
   }
   const result = await getHighestRatingMovie(req.body.movies);
+  console.log(
+    `getHighestRatingMovie - Movies=${
+      req.body.movies
+    } - Result=${JSON.stringify(result)}`
+  );
   res.status(200).send({ result });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    app
+      .listen(port, () => {
+        console.log(`Server is listening on port ${port}`);
+      })
+      .on("error", (err) => {
+        console.log("Server failed", {}, err);
+      });
+  } catch (err) {
+    console.log("Failed starting app", {}, err);
+  }
+};
+
+void start();
